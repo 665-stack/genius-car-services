00b1a1146467466701fadad487fcb5fec66a2331
MahmodulShahin <mahmodulshahin665@gmail.com>
2022-10-31 19:13:30 +0600

    Setup Dynamic Route and access route params

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import About from './Pages/About/About';
 import Home from './Pages/Home/Home/Home';
 import Footer from './Shared/Footer/Footer';
+import ServiceDetail from './Pages/ServiceDetail/ServiceDetail';
 
 function App() {
   return (
@@ -11,6 +12,9 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
+
+        <Route path='/service/:serviceId' element={<ServiceDetail></ServiceDetail>}></Route>
+
         <Route path='/about' element={<About></About>}></Route>
       </Routes>
       <Footer></Footer>
--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Service.css'
 const Service = ({ service }) => {
-    const { name, img, description, price } = service;
+    const { name, img, description, price, id } = service;
+    const navigate = useNavigate()
+    const navigateToServiceDetail = id => {
+        navigate(`/service/${id}`)
+    }
     return (
         <div className='service mt-3'>
             <img className='w-100' src={img} alt="" />
             <h2>{name}</h2>
             <p>Price: {price}</p>
             <p><small>{description}</small></p>
-            <button className=' border-0 btn-primary py-2 px-4 rounded-3'>Book: {name}</button>
+
+            <button onClick={() => navigateToServiceDetail(id)} className=' border-0 btn-primary py-2 px-4 rounded-3'>Book: {name}</button>
         </div>
     );
 };

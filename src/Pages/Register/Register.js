d113af3601dad904b5a68a876610c423c16a9293
MahmodulShahin <mahmodulshahin665@gmail.com>
2022-11-09 16:23:34 +0600

    change bootstrap form to vanila form in register component

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,7 +1,6 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './Register.css'
-import { Button, Form } from 'react-bootstrap';
 
 const Register = () => {
 
@@ -11,37 +10,32 @@ const Register = () => {
     }
 
     return (
-        <div className='container w-50 mx-auto'>
+        <div>
             <h2 className='text-primary text-center  mt-5'>Please Register</h2>
 
-            <Form onSubmit={handleRegister}>
+            <form onSubmit={handleRegister} className='register-form'>
+                <div>
+                    <input type="text" name='name' placeholder='Your Name' />
 
-                <Form.Group className="mb-3" controlId="formBasicName">
-                    <Form.Label>Your Name</Form.Label>
-                    <Form.Control type="text" placeholder="" />
+                    <br />
 
-                </Form.Group>
+                    <input type="email" name="name" id="" placeholder='Email Address' required />
 
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="" required />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
-                </Form.Group>
+                    <br />
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="" required />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-                <p className='mt-2'>Already have an account? <Link to='/login' className="text-danger text-decoration-none" >Please Login</Link></p>
-            </Form>
+                    <input type="password" name="password" id="" placeholder='Password' required />
+
+                    <br />
+
+                    <button type="submit">Register</button>
+
+                    <p className='text-center mt-1'>Already have an account? <Link to='/login' className="text-danger text-decoration-none" >Please Login</Link></p>
+
+                </div>
+
+
+            </form>
+
 
 
         </div>

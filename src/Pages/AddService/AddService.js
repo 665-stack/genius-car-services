6437bceeab7b3b79865a1af22133a08a6a535527
MahmodulShahin <mahmodulshahin665@gmail.com>
2023-02-12 07:46:28 +0600

    style form, fetch a url to post service data to the database

--- src/Pages/AddService/AddService.js
+++ src/Pages/AddService/AddService.js
@@ -2,20 +2,36 @@ import React from 'react';
 import { useForm } from "react-hook-form";
 
 const AddService = () => {
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    const onSubmit = data => console.log(data);
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => {
+        console.log(data);
+        const url = `http://localhost:5000/service`;
+        fetch(url, {
+            method: "POST",
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(result => {
+                console.log(result);
+            })
+    }
     return (
-        <div className='text-center'>
+        <div className='w-50 mx-auto'>
             <h2>You can add a service</h2>
 
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <input defaultValue="test" {...register("example")} />
+            <form className='d-flex flex-column' onSubmit={handleSubmit(onSubmit)}>
+                <input className='mb-2' placeholder='Service Name' {...register("name", { required: true, maxLength: 20 })} />
 
-                <input {...register("exampleRequired", { required: true })} />
+                <textarea className='mb-2' placeholder='Description' {...register("description")} />
 
-                {errors.exampleRequired && <span>This field is required</span>}
+                <input className='mb-2' placeholder='Price' type="number" {...register("price")} />
 
-                <input type="submit" />
+                <input className='mb-2' placeholder='Photo URL' type="text" {...register("img")} />
+
+                <input type="submit" value="Add Service" />
             </form>
         </div>
     );
